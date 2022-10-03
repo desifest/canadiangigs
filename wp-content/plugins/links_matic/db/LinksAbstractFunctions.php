354c6a95327eb92dd97f01a19b2ce56b56913ae6
Brahman <brahmnan@gmail.com>
2022-10-03 13:16:31 +0500

    refactor

--- wp-content/plugins/links_matic/db/LinksAbstractFunctions.php
+++ wp-content/plugins/links_matic/db/LinksAbstractFunctions.php
@@ -135,7 +135,7 @@ class LinksAbstractFunctions {
         
         if (!defined("DB_PREFIX_WP")) {
             global $table_prefix;
-            define(DB_PREFIX_WP, $table_prefix);
+            define("DB_PREFIX_WP", $table_prefix);
         }
 
         $data = '';
